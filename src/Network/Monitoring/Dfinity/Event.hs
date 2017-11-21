ff20842979119ade83c490bf0eaac1167e878584
Derek Chiang <me@derekchiang.com>
2017-11-21 13:52:18 -0800

    Stop mingling with service names

--- src/Network/Monitoring/Dfinity/Event.hs
+++ src/Network/Monitoring/Dfinity/Event.hs
@@ -1,46 +1,43 @@
 module Network.Monitoring.Dfinity.Event (sendBlock, recvBlock, blockLatency, livenessEvent) where
 
-import Data.Monoid (Endo, (<>))
-import Network.Monitoring.Riemann.Event.Monoid
-import qualified Network.Monitoring.Riemann.Event as RE
+import           Data.Monoid                             (Endo, (<>))
+import qualified Network.Monitoring.Riemann.Event        as RE
+import           Network.Monitoring.Riemann.Event.Monoid
 
 -- Timestamp and Duration are both in milliseconds
-type Timestamp = Int
+-- type Timestamp = Int
 type Duration = Int
 type Height = Int
 type Node = Int
-type Size = Int
+-- type Size = Int
 type Rank = Int
-type Percentage = Int
+-- type Percentage = Int
 type Kind = String
 
 livenessEvent :: Event
 livenessEvent = RE.ok "liveness"
 
-toService :: Kind -> Height -> String
-toService kind height = kind ++ "-" ++ show height
-
 sendBlock :: Node -> Height -> Rank -> Event
 sendBlock node height rank = let kind = "send-block" in
-  ok (toService kind height) $ nhrk node height rank kind 
+  ok "send-block" $ nhrk node height rank kind
 
 recvBlock :: Node -> Height -> Rank -> Duration -> Event
 recvBlock node height rank dt = let kind = "recv-block" in
-  ok (toService kind height) $ metric dt <> nhrk node height rank kind 
+  ok "recv-block" $ metric dt <> nhrk node height rank kind
 
 blockLatency :: Node -> Height -> Duration -> Event
 blockLatency node height dt = let kind = "block-latency" in
-  ok (toService kind height) $ metric dt <> nhk node height kind 
+  ok "block-latency" $ metric dt <> nhk node height kind
 
 nhk :: Node -> Height ->  Kind -> Endo Event
-nhk node height kind = 
+nhk node height kind =
   attributes [ attribute "node" $ Just (show node)
              , attribute "height" $ Just (show height)
              , attribute "kind" $ Just kind
              ]
 
 nhrk :: Node -> Height -> Rank -> Kind -> Endo Event
-nhrk node height rank kind = 
+nhrk node height rank kind =
   attributes [ attribute "node" $ Just (show node)
              , attribute "height" $ Just (show height)
              , attribute "rank" $ Just (show rank)
